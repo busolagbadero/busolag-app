1f2e7d45ac5beb9b9f33baca0472aef483198716
gbadero busola <gbadero0231@gmail.com>
2024-02-22 05:21:24 +0000

    Update 2 files
    - /uploadpython.py
    - /.gitlab-ci.yml

---
+++ uploadpython.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+
+file_name = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'njsscan.sarif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
+elif file_name == 'retire.json':
+    scan_type = 'Retire.js Scan'
+elif file_name == 'trivy.json':
+    scan_type = 'Trivy Scan'
+
+
+headers = {
+    'Authorization': 'Token 5f3afc79fabe1a9ed24c204af026cd9b76420ac1'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+    'active': True,
+    'verified': True,
+    'scan_type': scan_type,
+    'minimum_severity': 'Low',
+    'engagement': 32
+}
+
+files = {
+    'file': open(file_name, 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code == 201:
+    print('Scan results imported successfully')
+else:
+    print(f'Failed to import scan results: {response.content}')
